640a870bd9d2811d405e00062b9d795e1fe388df
user <aqusay06@gmail.com>
2024-12-30 17:27:22 +0200

    final the brain is draw

--- code/bundle/ImageCraft.py
+++ code/bundle/ImageCraft.py
@@ -41,3 +41,35 @@ def create_p300_image(highlight_row=None, highlight_col=None, title="P300 Spelle
     ax.axis('off')
     plt.title(title, fontsize=18)
     plt.show()
+
+
+def print_data(signals, word, contributor_selected, sampling_frequency):
+
+    recording_duration= (len(signals)) * (len(signals[0])) / (sampling_frequency * 60)
+    trials = len(word[0])
+
+
+    print("{:<15} {:<20} {:<20} {:<10} {:<30}".format(
+        "Contributor", "Sampling Freq. (Hz)", "Recording (min)", "Trials", "Spelled Word"
+    ))
+    print("=" * 110)
+
+    # Break the Spelled Word into chunks of 30 characters
+    word_chunks = [''.join(word)[i:i + 30] for i in range(0, len(''.join(word)), 30)]
+
+    # Print the first line of the row
+    print("{:<15} {:<20} {:<20} {:<10} {:<30}".format(
+    contributor_selected,
+    "{:.2f}".format(sampling_frequency),
+    "{:.2f}".format(recording_duration),
+    trials,
+    word_chunks[0] if word_chunks else ""
+    ))
+
+    # Print the remaining lines for the Spelled Word, if any
+    for chunk in word_chunks[1:]:
+        print("{:<15} {:<20} {:<20} {:<10} {:<30}".format(
+            "", "", "", "", chunk
+        ))
+
+    print()
